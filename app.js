826b891373d923dcfbd38cdc27611c7693870089
Ishaan572 <74988191+Ishaan572@users.noreply.github.com>
2020-12-09 23:35:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,41 +1,121 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <link href="https://fonts.googleapis.com/css?family=Lato:100,300,600" rel="stylesheet" type="text/css">
-        <link href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet" type="text/css">
-        <link type="text/css" rel="stylesheet" href="style.css">
-        
-        <title>Pig Game</title>
-    </head>
+/*
+GAME RULES:
 
-    <body>
-        <div class="wrapper clearfix">
-            <div class="player-0-panel active">
-                <div class="player-name" id="name-0">Player 1</div>
-                <div class="player-score" id="score-0">43</div>
-                <div class="player-current-box">
-                    <div class="player-current-label">Current</div>
-                    <div class="player-current-score" id="current-0">11</div>
-                </div>
-            </div>
-            
-            <div class="player-1-panel">
-                <div class="player-name" id="name-1">Player 2</div>
-                <div class="player-score" id="score-1">72</div>
-                <div class="player-current-box">
-                    <div class="player-current-label">Current</div>
-                    <div class="player-current-score" id="current-1">0</div>
-                </div>
-            </div>
-            
-            <button class="btn-new"><i class="ion-ios-plus-outline"></i>New game</button>
-            <button class="btn-roll"><i class="ion-ios-loop"></i>Roll dice</button>
-            <button class="btn-hold"><i class="ion-ios-download-outline"></i>Hold</button>
-            
-            <img src="dice-5.png" alt="Dice" class="dice">
-        </div>
-        
-        <script src="app.js"></script>
-    </body>
-</html>
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+var scores , roundScore , activePlayer , rollBtn , holdBtn , newBtn , gamePlaying;
+function init() {
+  scores = [0, 0];
+  activePlayer = 0;
+  roundScore = 0;
+  gamePlaying = true;
+  
+  document.querySelector('.dice').style.display = 'none';
+
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+  document.getElementById('name-0').textContent = 'Player 1';
+  document.getElementById('name-1').textContent = 'Player 2';
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+  document.querySelector('.player-0-panel').classList.add('active');
+}
+
+init();
+
+rollBtn = document.querySelector(".btn-roll")
+  holdBtn = document.querySelector('.btn-hold');
+  newBtn = document.querySelector('.btn-new')
+
+function changePlayer(){//Next player
+  activePlayer === 1 ? activePlayer = 0 : activePlayer = 1;
+  roundScore = 0;
+
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+
+  //document.querySelector('.player-0-panel').classList.remove('active');
+  //document.querySelector('.player-1-panel').classList.add('active');
+
+  document.querySelector('.dice').style.display = 'none';}
+
+
+
+
+function rollDice(){
+ if (gamePlaying){//Random number
+ var dice =Math.floor(Math.random()*6 + 1);
+//Display result
+var diceImg = document.querySelector(".dice");
+diceImg.style.display = 'block';
+diceImg.src = 'dice-' + dice + '.png';
+//Add to current score if dice is not 1
+if( dice !== 1){
+  //Add score
+  roundScore = roundScore + dice;
+  document.querySelector('#current-' + activePlayer).textContent = roundScore ;
+}
+else{
+  //Next score
+changePlayer();
+
+}
+}}
+var nonac = 0;
+
+
+
+
+function holdDice (){
+if (gamePlaying){
+  //Add current score to global score
+scores[activePlayer] += roundScore;
+document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer] ;
+//Update UI
+if(activePlayer == 1){
+  activePlayer = 0;
+}
+else{activePlayer = 1}
+if(activePlayer == 1){
+  nonac = 0;
+}
+else{nonac = 1}
+
+
+roundScore = 0;
+document.getElementById('current-0').textContent = '0'; 
+document.getElementById('current-1').textContent = '0'; 
+document.querySelector('.player-0-panel').classList.toggle('active');
+document.querySelector('.player-1-panel').classList.toggle('active');
+document.querySelector(".dice").style.display = 'none';
+if(scores[nonac] >= 100){
+  document.querySelector('#name-' + nonac).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + nonac + '-panel').classList.add('winner');
+            document.querySelector('.player-' + nonac + '-panel').classList.remove('active');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active')
+            gamePlaying = false;
+}}
+}
+function newGame(){
+init();
+
+}
+
+
+newBtn.addEventListener('click' , newGame)
+rollBtn.addEventListener('click', rollDice) ;
+holdBtn.addEventListener('click' , holdDice);
